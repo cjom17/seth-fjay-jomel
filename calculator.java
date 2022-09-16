cb7a245d1c8a256d063961962172c064ffa18b5a
cjom17 <nztechnophile.1@gmail.com>
2022-09-16 17:14:17 +0800

    added the addition feature

--- calculator.java
+++ calculator.java
@@ -40,7 +40,24 @@ public class Calculator {
     }
     
 
-    // For Addition >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+   
+    // This is for the addition function
+    public static void addition(){
+
+        int nOne, nTwo;
+        Scanner kb = new Scanner(System.in);
+
+        System.out.println("Addition");
+
+        System.out.print("\nFirst Number: ");
+        nOne = kb.nextInt();
+
+        System.out.print("\nSecond Number: ");
+        nTwo = kb.nextInt();
+
+        kb.close();
+        System.out.println("\nSum: " + nOne + " + " + nTwo + " = " + (nOne + nTwo));
+    }
 
 
 
