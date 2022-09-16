fd56b6ca2a49a73898357944ea829c960ee9f721
cjom17 <nztechnophile.1@gmail.com>
2022-09-16 17:05:07 +0800

    update main file

--- calculator.java
+++ calculator.java
@@ -1,29 +1,46 @@
-
+import java.util.Scanner;
 // For scannerrrrrrrrrrrrr >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>r
 
 
 
 public class Calculator {
     // For Display >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+
+        System.out.println("Simple Calculator");
+
+        System.out.println("\nHere are your options:");
+        System.out.println("\n1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Division");
+        System.out.println("4. Multiplication");
+
+        System.out.print("\nWhat would you like to do?: ");
+        int choice = kb.nextInt();
+        System.out.println();
+
+
+        if (choice == 1){
+            addition();
+        }
+        else if (choice == 2){
+            subtraction();
+        }
+        else if (choice == 3){
+            division();
+        }
+        else if (choice == 4){
+            multiplication();
+        }
+
+        System.out.println();
+        kb.close();
+    }
     
 
     // For Addition >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    public static void addition(){
-
-        int nOne, nTwo;
-        Scanner kb = new Scanner(System.in);
-
-        System.out.println("Addition");
-
-        System.out.print("\nFirst Number: ");
-        nOne = kb.nextInt();
-
-        System.out.print("\nSecond Number: ");
-        nTwo = kb.nextInt();
-
-        kb.close();
-        System.out.println("\nSum: " + nOne + " + " + nTwo + " = " + (nOne + nTwo));
-    }
 
 
 
