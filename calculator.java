b06040262970dd028632a1090d888defc6fdfdc3
cjom17 <nztechnophile.1@gmail.com>
2022-09-16 17:20:38 +0800

    update

--- calculator.java
+++ calculator.java
@@ -42,7 +42,22 @@ public class Calculator {
 
    
     // This is for the addition function
- 
+    public static void addition(){
+
+        int nOne, nTwo;
+        Scanner kb = new Scanner(System.in);
+
+        System.out.println("Addition");
+
+        System.out.print("\nFirst Number: ");
+        nOne = kb.nextInt();
+
+        System.out.print("\nSecond Number: ");
+        nTwo = kb.nextInt();
+
+        kb.close();
+        System.out.println("\nSum: " + nOne + " + " + nTwo + " = " + (nOne + nTwo));
+    }
 
 
 
