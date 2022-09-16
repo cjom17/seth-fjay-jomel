9eea8982bb8a3cb431b680e0aca3269eb13bb85b
ferolinofjay <21103845@usc.edu.ph>
2022-09-16 17:35:02 +0800

    added a division feature

--- calculator.java
+++ calculator.java
@@ -98,6 +98,20 @@ public class Calculator {
 
 
     // For Division >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
+    public static void division(){
+        int nOne, nTwo;
+        Scanner kb = new Scanner(System.in);
+  
+        System.out.println("Division");
+  
+        System.out.print("\nFirst Number: ");
+        nOne = kb.nextInt();
+  
+        System.out.print("\nSecond Number: ");
+        nTwo = kb.nextInt();
+  
+        kb.close();
+        System.out.println("\nSum: " + nOne + " / " + nTwo + " = " + (nOne / nTwo));
+    }
 
 }
