553867d7246fdfcfd6625aaaa1974535d93d9162
Seth Obenita <21103829@usc.edu.ph>
2022-09-16 17:25:28 +0800

    added the multiplication feature

--- calculator.java
+++ calculator.java
@@ -80,6 +80,21 @@ public class Calculator {
 
 
     // For Multiplication >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    public static void multiplication(){
+      int nOne, nTwo;
+      Scanner kb = new Scanner(System.in);
+
+      System.out.println("Multiplication");
+
+      System.out.print("\nFirst Number: ");
+      nOne = kb.nextInt();
+
+      System.out.print("\nSecond Number: ");
+      nTwo = kb.nextInt();
+
+      kb.close();
+      System.out.println("\nSum: " + nOne + " x " + nTwo + " = " + (nOne * nTwo));
+  }
 
 
     // For Division >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
