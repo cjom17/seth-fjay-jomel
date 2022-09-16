56d7de990a488d8a82b7433a4c345009f1a0cb7c
cjom17 <nztechnophile.1@gmail.com>
2022-09-16 17:17:27 +0800

    added the subtraction feature

--- calculator.java
+++ calculator.java
@@ -42,12 +42,16 @@ public class Calculator {
 
    
     // This is for the addition function
-    public static void addition(){
+ 
 
+
+
+    // For Subtraction >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    public static void subtraction(){
         int nOne, nTwo;
         Scanner kb = new Scanner(System.in);
 
-        System.out.println("Addition");
+        System.out.println("Subtraction");
 
         System.out.print("\nFirst Number: ");
         nOne = kb.nextInt();
@@ -56,12 +60,8 @@ public class Calculator {
         nTwo = kb.nextInt();
 
         kb.close();
-        System.out.println("\nSum: " + nOne + " + " + nTwo + " = " + (nOne + nTwo));
+        System.out.println("\nSum: " + nOne + " - " + nTwo + " = " + (nOne - nTwo));
     }
-
-
-
-    // For Subtraction >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
     // For Multiplication >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
